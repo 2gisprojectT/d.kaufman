16a20e51978189e1e0a1dbbbf80dcc4434d8087b
Dmitry Kaufman <KaufmanDmitry@gmail.com>
2015-04-19 19:41:37 +0600

    Добавил методы

--- 30.03/main.py
+++ 30.03/main.py
@@ -18,3 +18,45 @@ class leo():
                       'hungry' :{'action' : 'sleep',
                                  'status' : 'null'}}
     }
+
+    def inp(self):
+        #Сопоставления числа и входного значения - список допустимых введенных чисел
+        #К каждому входному параметру из второго поля соспоставлено число из первого, для удобства пользователя
+        comprasion = {   1 : 'antelope',
+                         2 : 'hunter',
+                         3 : 'tree',
+                         4 : 'exit'}
+        #Продолжаем цикл, пока не введен корректо номер
+        while True:
+            rez = input('Введите номер входного параметра:\n'
+                            '1) Антилопа\n'
+                            '2) Охотник\n'
+                            '3) Дерево\n'
+                            '4) Выход\n')
+            if len(rez) == 0:
+                 print('Вы не ввели номер\n')
+            else:
+                rez = int(rez)
+                r = comprasion.get(rez)
+                if r != None:
+                    return r
+                else:
+                    print('Вы ввели неверный номер\n')
+
+
+    def dialog(self):
+        rez = self.inp()
+        while rez != 'exit':
+            #Если не надо менять статус, меняем только действие
+            if self.diff[rez][self.data['status']]['status'] == 'null':
+                self.data.update({'action' : self.diff[rez][self.data['status']]['action']})
+            else:
+            #Иначе меняем и статус и действие
+                self.data.update(self.diff[rez][self.data['status']])
+            print ('Новые значения:', self.data)
+            rez = self.inp()
+
+l = leo()
+l.dialog()
+
+
